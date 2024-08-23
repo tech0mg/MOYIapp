43fdf7c075023d27b2a53d863d85bfdd97c46b79
daisuke <daisuke.imahase@gmail.com>
2024-08-23 17:00:38 +0900

    page1を作成しました。

--- app_pages/page1.py
+++ app_pages/page1.py
@@ -2,5 +2,20 @@
 import streamlit as st
 
 def show_page():
-    st.title("Page 1")
-    st.write("This is the content of Page 1.")
+    st.title("旅行いっちゃう？アプリ")
+    st.write("旅行名を登録します。ワクワクする名前を記載してください。")
+    
+    # 旅行名の入力
+    travel_name = st.text_input("旅行名を設定してください。")
+    
+    # 登録ボタン
+    if st.button("登録"):
+        if travel_name:
+            st.success(f"旅行計画『{travel_name}』を登録しました！")
+            st.success(f"次のページで行きたいところを登録してください！")
+        else:
+            st.error("旅行名を入力してください。")
+
+if __name__ == "__main__":
+    show_page()
+
